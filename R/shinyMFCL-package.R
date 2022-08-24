8f64a4d1f7a6da5851939cc739f88dd4f6e721db
Arni Magnusson <thisisarni@gmail.com>
2022-08-24 13:45:12 +1100

    Add package help page

---
+++ R/shinyMFCL-package.R
@@ -0,0 +1,28 @@
+#' @docType package
+#'
+#' @name shinyMFCL-package
+#'
+#' @aliases shinyMFCL
+#'
+#' @title Tools to Create Shiny Apps for MULTIFAN-CL Assessments
+#'
+#' @description
+#' Tools that support the creation of Shiny apps to view the data and results
+#' from MULTIFAN-CL stock assessments.
+#'
+#' @details
+#' \emph{Prepare data:}
+#' \tabular{ll}{
+#'   \code{\link{mapFisheries}} \tab map fisheries from labels file
+#' }
+#'
+#' @author Finlay Scott and Arni Magnusson.
+#'
+#' @references
+#' \url{https://github.com/PacificCommunity/ofp-sam-shinyMFCL}
+#'
+#' @seealso
+#' \pkg{shinyMFCL} is used in combination with the packages \pkg{FLR4MFCL} and
+#' \pkg{data.table}.
+
+NA
