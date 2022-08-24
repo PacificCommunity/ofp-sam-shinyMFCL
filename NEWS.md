7872f99bca7f666cf65fcd1697a699b6f3d276db
Arni Magnusson <thisisarni@gmail.com>
2022-08-24 13:52:00 +1100

    Add mapFisheries()

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# shinyMFCL 1.0.0 (2022-08-24)
+
+* Initial release.
---
+++ R/mapFisheries.R
@@ -0,0 +1,27 @@
+#' Map Fisheries
+#'
+#' Map fisheries using information from the `labels.tmp` file.
+#'
+#' @param file file containing fisheries labels.
+#'
+#' @return
+#' Data frame containing the columns \code{fishery_name}, \code{fishery},
+#' \code{region}, and \code{group}.
+#'
+#' @author Arni Magnusson.
+#'
+#' @seealso
+#' \code{\link{shinyMFCL-package}} gives an overview of the package.
+#'
+#' @examples
+#' \dontrun{
+#' mapFisheries()
+#' mapFisheries("../../../MFCL/stepwise/09_IdxNoeff/labels.tmp")
+#' }
+#'
+#' @export
+
+mapFisheries <- function(file="labels.tmp")
+{
+  NULL
+}
